4b61abb266bdd0169ce731c152fb8547aa9c0be6
Dhruv Bavadiya <Dhruv.Bavadiya@ibm.com>
2025-11-14 16:26:20 +0530

    modified link to markdown

--- variables.tf
+++ variables.tf
@@ -200,7 +200,7 @@ variable "bucket_storage_class" {
 }
 
 variable "object_locking_enabled" {
-  description = "Enable object lock to keep data immutable for the retention period. This will also enable object versioning on the bucket. [Learn More](https://cloud.ibm.com/docs/cloud-object-storage?topic=cloud-object-storage-immutable)"
+  description = "Enable object lock to keep data immutable for the retention period. This will also enable object versioning on the bucket. [Learn more](https://cloud.ibm.com/docs/cloud-object-storage?topic=cloud-object-storage-immutable)"
   type        = bool
   default     = false
 }
@@ -428,7 +428,7 @@ variable "allowed_network_zone_name" {
         can(regex("^[0-9A-Za-z \\-_]+$", var.allowed_network_zone_name))
       ])
     ])
-    error_message = "Value should be a valid account id with 1-128 characters"
+    error_message = "Value should be a valid network zone name with 1-128 characters"
   }
 }
 
