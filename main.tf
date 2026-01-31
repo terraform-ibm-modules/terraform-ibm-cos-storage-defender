4d3c521e8dc9837501fa99c6b89b34fe44bff4f3
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-31 20:58:36 +0000

    fix(deps): update terraform terraform-ibm-modules/cloud-logs/ibm to v1.10.35

--- main.tf
+++ main.tf
@@ -216,7 +216,7 @@ resource "ibm_iam_authorization_policy" "cos_policy" {
 
 module "cloud_logs" {
   source                        = "terraform-ibm-modules/cloud-logs/ibm"
-  version                       = "1.10.34"
+  version                       = "1.10.35"
   depends_on                    = [ibm_iam_authorization_policy.cos_policy]
   instance_name                 = local.cloud_log_instance_name
   plan                          = var.cloud_logs_plan
